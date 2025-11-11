cc39dcb8b0a4e9421ba89bc081a22da9eff637ca
JPedro759 <joaopedromeloo03@gmail.com>
2025-11-11 13:05:43 -0300

    refactor: Updating project.

--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NewsletterResponse } from '../interfaces/newsletter.interface';
 
@@ -7,14 +7,13 @@ import { NewsletterResponse } from '../interfaces/newsletter.interface';
   providedIn: 'root',
 })
 export class NewsletterService {
-  private endpointUrl =
+  #http = inject(HttpClient);
+  #endpointUrl =
     'https://faed47pcwb7biktidlecuafuty0aegep.lambda-url.us-east-1.on.aws/';
-
-  constructor(private http: HttpClient) {}
 
   sendData(name: string, email: string): Observable<NewsletterResponse> {
     const data = { name, email };
 
-    return this.http.post<NewsletterResponse>(this.endpointUrl, data);
+    return this.#http.post<NewsletterResponse>(this.#endpointUrl, data);
   }
 }
